49ee698a279863c52b377e42feb70749340bc3d5
J3alou5 <karliskade@gmail.com>
2024-09-06 11:32:53 +0300

    Ex29

---
+++ src/week2/Ex29.java
@@ -0,0 +1,12 @@
+package week2;
+
+public class Ex29 {
+    public static void main(String[] args) {
+        int number = 2;
+
+        while (number <= 100) {
+            System.out.println(number);
+            number = number + 2;  // number++ means the same as number = number + 1
+        }
+    }
+}
