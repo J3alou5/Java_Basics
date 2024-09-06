0fbf6bf92eed89f2e7b7ffbcbaa6c2d34cb635c2
J3alou5 <karliskade@gmail.com>
2024-09-06 10:28:51 +0300

    Ex5

--- src/week1/Ex5.java
+++ src/week1/Ex5.java
@@ -1,4 +1,13 @@
 package week1;
 
 public class Ex5 {
+    public static void main(String[] args) {
+        int secMin = 60;
+        int minHr = 60;
+        int hrDay = 24;
+        int dayYear = 365;
+
+        int secYear = secMin * minHr * hrDay * dayYear;
+        System.out.println("There are " + secYear + " seconds in a year");
+    }
 }
