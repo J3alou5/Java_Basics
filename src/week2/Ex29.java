b5ece97b699ed7e48857fa493a6560e7c436e0a6
J3alou5 <karliskade@gmail.com>
2024-09-06 11:52:37 +0300

    Ex29 n ex30

--- src/week2/Ex29.java
+++ src/week2/Ex29.java
@@ -3,10 +3,10 @@ package week2;
 public class Ex29 {
     public static void main(String[] args) {
         int number = 2;
-
         while (number <= 100) {
             System.out.println(number);
-            number = number + 2;  // number++ means the same as number = number + 1
+            number = number + 2;
         }
+
     }
 }
---
+++ src/week2/Ex30.java
@@ -0,0 +1,16 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Ex30 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Up to what number? ");
+        int number = Integer.parseInt(reader.nextLine());
+        int i = 1;
+        while (i <= number) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
